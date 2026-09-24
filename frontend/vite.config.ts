3ec13f7e5405037854280e4a80b218a07dc4690c
Knessetandnest <knessetandnestfoundation@gmail.com>
2026-09-24 10:08:18 +0530

    Prepare frontend for GitHub Pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-export default defineConfig({ plugins:[react()], server:{ proxy:{ '/api':'http://127.0.0.1:5000', '/media':'http://127.0.0.1:5000' } } });
+export default defineConfig({
+  base: '/knesset-nest/',
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://127.0.0.1:5000',
+      '/media': 'http://127.0.0.1:5000'
+    }
+  }
+});
